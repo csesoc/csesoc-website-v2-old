00aeaef2b2e815c1654b71d5146b51a64fe870da
Rafi Blecher <rafiblecher@gmail.com>
2014-01-16 11:19:16 +1100

    Fixed typo in static.sh, removed redundant requirements folder

--- static.sh
+++ static.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-path=`readlink -m "$0"` | sed 's/\/test$//'
+path=`readlink -m "$0" | sed 's/\/static.sh$//'`
 if [[ `grep -Fq "STATIC_ROOT" "$path/local_settings.py" 2> /dev/null` ]]
 then 
   echo "STATIC_ROOT already in local_settings.py. Please edit manually."
